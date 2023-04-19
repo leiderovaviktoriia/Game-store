ddb11535bc7079c95dcd07e2b1ff14162a0f9167
Viktoriia Leiderova <Liderv1ka@yandex.ru>
2023-04-19 16:36:25 +0300

    Second change

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const btnCloseModal = document.querySelector(`.close-modal`);
 const overlay = document.querySelector(`.overlay`);
 const modalWindow = document.querySelector(`.modal`);
 
+
+const btnChooseTeam = document.querySelector(`#button_team`);
+const btnCloseTeam = document.querySelector(`.closeing-modal`);
+const modalWindowTeam = document.querySelector(`.modaling`);
+const overlayTeam = document.querySelector(`.overlaying`);
+
 btnOpenModal.addEventListener("click", function () {
   console.log("click");
   overlay.classList.toggle("hidden");
@@ -17,4 +23,23 @@ btnCloseModal.addEventListener("click", function () {
 overlay.addEventListener("click", function () {
   overlay.classList.toggle("hidden");
   modalWindow.classList.toggle("hidden");
+});
+
+
+
+
+btnChooseTeam.addEventListener("click", function () {
+  console.log("click");
+  overlayTeam.classList.toggle("hidden");
+  modalWindowTeam.classList.toggle("hidden");
+});
+
+btnCloseTeam.addEventListener("click", function () {
+  overlayTeam.classList.toggle("hidden");
+  modalWindowTeam.classList.toggle("hidden");
+});
+
+overlayTeam.addEventListener("click", function () {
+  overlayTeam.classList.toggle("hidden");
+  modalWindowTeam.classList.toggle("hidden");
 });
